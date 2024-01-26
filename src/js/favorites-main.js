6ecbe61ab911b0539c9a924a0b5fc62fa7ed743c
nhetsko <nadiahecko@gmail.com>
2024-01-26 17:41:47 +0200

    add quote-js favorites-main-js

---
+++ src/js/favorites-main.js
@@ -0,0 +1,19 @@
+import { getQuote } from '../helpers/locatStorage.js'; 
+
+async function updateQuoteOfTheDay() {
+  const quoteContainer = document.querySelector('.favorites-info-container');
+  const quoteTextElement = document.getElementById('favorites-text');
+  const authorElement = document.querySelector('.favorites-author');
+
+  try {
+    const { author, quote } = await getQuote();
+
+    quoteTextElement.textContent = quote;
+    authorElement.textContent = `${author}`;
+
+  } catch (error) {
+    console.error('Error fetching or updating the quote:', error);
+  }
+}
+
+updateQuoteOfTheDay();
