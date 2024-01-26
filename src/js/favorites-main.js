90bdea77d925018cc83724095f7dc4f7a575dfff
nhetsko <nadiahecko@gmail.com>
2024-01-26 23:15:03 +0200

    add quote and favorites-main js

--- src/js/favorites-main.js
+++ src/js/favorites-main.js
@@ -1,4 +1,24 @@
-import { getQuote } from '../helpers/locatStorage.js'; 
+// import { getQuote } from '../helpers/locatStorage.js'; 
+
+// async function updateQuoteOfTheDay() {
+//   const quoteContainer = document.querySelector('.favorites-info-container');
+//   const quoteTextElement = document.getElementById('favorites-text');
+//   const authorElement = document.querySelector('.favorites-author');
+
+//   try {
+//     const { author, quote } = await getQuote();
+
+//     quoteTextElement.textContent = quote;
+//     authorElement.textContent = `${author}`;
+
+//   } catch (error) {
+//     console.error('Error fetching or updating the quote:', error);
+//   }
+// }
+
+// updateQuoteOfTheDay();
+
+import { getQuote } from '../helpers/locatStorage.js';
 
 async function updateQuoteOfTheDay() {
   const quoteContainer = document.querySelector('.favorites-info-container');
@@ -16,4 +36,6 @@ async function updateQuoteOfTheDay() {
   }
 }
 
-updateQuoteOfTheDay();
+document.addEventListener('DOMContentLoaded', () => {
+  updateQuoteOfTheDay();
+});
