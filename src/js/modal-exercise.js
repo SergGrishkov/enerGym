5d9232b402098706d303806cc1783f1b7d98d786
SergGrishkov <serg.grishkov@gmail.com>
2024-01-28 13:19:00 +0200

    Update modal-exercise.js

--- src/js/modal-exercise.js
+++ src/js/modal-exercise.js
@@ -48,7 +48,7 @@ async function createMarkupModalEx(exerciseId) {
                     aria-label="modal favorite icon"
                   >
                     <use
-                      href='./enerGym/assets/sprite.svg#icon-remove-favorites'
+                      href='./assets/sprite.svg#icon-remove-favorites'
                     ></use>
                   </svg>
                 </p>
@@ -68,7 +68,7 @@ async function createMarkupModalEx(exerciseId) {
                     aria-label="modal favorite icon"
                   >
                     <use
-                      href='./enerGym/assets/sprite.svg#icon-add-favorites'
+                      href='./assets/sprite.svg#icon-add-favorites'
                     ></use>
                   </svg>
                 </p>
