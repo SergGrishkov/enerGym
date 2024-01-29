56a5dbe8a6c1e3a7b9350cce5640ac0d10dc0dee
SergGrishkov <43968618+SergGrishkov@users.noreply.github.com>
2024-01-29 08:40:54 +0000

    Deploying to gh-pages from @ SergGrishkov/enerGym@a7f766251371c2d68efd1a13a8e26a113c2ad587 🚀

--- assets/quote-caeaaee9.js
+++ assets/quote-caeaaee9.js
@@ -2,4 +2,4 @@ var D=Object.defineProperty;var j=(r,e,t)=>e in r?D(r,e,{enumerable:!0,configura
   <p id="favorites-text">${e}</p>
   <h3 class="favorites-author">${r}</h3>
   `}export{I as E,z as f,Z as g,K as i,Y as r};
-//# sourceMappingURL=quote-aa41b0c5.js.map
+//# sourceMappingURL=quote-caeaaee9.js.map
